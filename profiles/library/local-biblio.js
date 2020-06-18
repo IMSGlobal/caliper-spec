cff1b741c1583542eadd2bd2e8d730bc389a2cbd
Anthony Whyte <anthwhyte@gmail.com>
2020-06-18 17:33:29 -0400

    Add draft Library profile.

---
+++ profiles/library/local-biblio.js
@@ -0,0 +1,25 @@
+var _localBiblio = {
+  "CALIPER-LIBRARYUSE-12": {
+    "title": "Caliper Analytics® Library Profile 1.2-extension",
+    "href": "https://www.imsglobal.org/spec/caliper-libraryuse/v1p2-extension/",
+    "publisher": "IMS Global Learning Consortium",
+    "authors": [
+        "Whyte, Anthony",
+        "Botimer, Noah",
+        "Korner, Sebastian",
+        "Pelaprat, Etienne",
+        "Varnum, Ken",
+        "York, Maurice"
+    ]
+  },
+  "CALIPER-LIBRARY-12-CONTEXT": {
+    "title": "Caliper Analytics® Library Use Profile Context 1.2 Extension",
+    "href": "https://purl.imsglobal.org/spec/caliper-libraryuse/v1p2-extension/context/",
+    "publisher": "IMS Global Learning Consortium"
+  },
+  "CALIPER-LIBRARY-12-ERRATA": {
+    "title": "Caliper Analytics® Library Use Profile 1.2 Extension Errata",
+    "href": "https://www.imsglobal.org/spec/caliper-libraryuse/v1p2-extension/errata/",
+    "publisher": "IMS Global Learning Consortium"
+  }
+};
